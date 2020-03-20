77d94995eabc4409c59b6c7f0abecf50a4f9cdb5
Scott Braaten <sbraaten95@gmail.com>
2020-03-19 23:38:37 -0400

    about me done

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Hero from "../components/Hero.js";
+import Content from "../components/Content.js";
+
 function AboutPage(props) {
-  return <p>AboutPage Works!</p>;
+  return (
+    <div>
+      <Hero title={props.title} />
+
+      <Content>{/** Personal content */}</Content>
+    </div>
+  );
 }
 
 export default AboutPage;
